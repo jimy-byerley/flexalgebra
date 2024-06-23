39caf86229bc8db7397c295ef4dfa7194daefb44
jimy-byerley <jimy.byerley@gmail.com>
2024-06-23 15:47:04 +0200

    added crate-level introduction and a readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+/*!
+	*Flexible Linear algebra library working in arbitrary dimension*
+
+	This crate propose an alternative strongly inspired from [nalgebra](https://docs.rs/nalgebra/latest/nalgebra/index.html) that aims to reduce the boilerplate its mentor has for working with generics, while keeping its flexiblity.
+	The result is as much simpler and comprehensive library with a shorter codebase
+
+	It mainly features the **[Matrix]** struct, which represent a *dense* matrix with any *strided* memory. And providing common matrix operations *as its methods*.
+	
+	Of course this crate address both the case of small-dimension objects better used as static-allocated objects and the case of big-dimension arrays usually handled with dynamic allocation
+
+	## Goals
+	
+	- `no-std` feature, but not started yet
+	- as feature-complete as [nalgebra](https://docs.rs/nalgebra/latest/nalgebra/index.html) and [numpy](https://numpy.org/doc/stable/reference/routines.linalg.html)
+	- as fast as size-specific libraries
+	
+	## Comparison
+		
+	At contrary to *nalgebra*, `Matrix` is not a one-size-fit-all struct with tons of generic arguments. It is a wrapper type that can wrap any array storage type (even not from *flexalgebra*). The type and shape parameters only depend on the array storage which can be user provided.
+
+	This means any user-type can be used as a matrix compatible with the rest of the *flexalgebra* types
+*/
+
 pub mod prelude;
 pub mod matrix;
 mod operators;
